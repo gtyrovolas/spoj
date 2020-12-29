0104ab54253353b5343bbf51e50ff465bc994c98
Giannis Tyrovolas <johntyro@hotmail.com>
2020-12-29 03:37:23 +0200

    conforms with pep8

--- solutions/arbitrag.py
+++ solutions/arbitrag.py
@@ -1,6 +1,6 @@
 
 def inp(labels, edges, n):
-    
+
     for i in range(n):
         labels.append(input())
     m = int(input())
@@ -10,6 +10,7 @@ def inp(labels, edges, n):
         rate = float(rate)
         edges.append((v_1, v_2, rate))
 
+
 def hasNegativeCycle(labels, edges, n):
     best_rates = dict.fromkeys(labels, 0)
     st = labels[0]
@@ -18,24 +19,24 @@ def hasNegativeCycle(labels, edges, n):
     for _ in range(n):
         for (v_1, v_2, rate) in edges:
             best_rates[v_2] = max(best_rates[v_1] * rate, best_rates[v_2])
-    
+
     found = False
     for (v_1, v_2, rate) in edges:
         if best_rates[v_1] * rate > best_rates[v_2]:
             found = True
             break
-            
+
     return found
 
 
 def main():
-    
+
     cnt = 0
     while True:
         n = int(input())
         if n == 0:
             break
-        
+
         cnt += 1
         edges = []
         labels = []
@@ -45,5 +46,6 @@ def main():
         print("Case ", cnt, ": Yes" if ans else ": No", sep="")
 
         input()
+
 
 main()
